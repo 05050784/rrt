44fb8097899620e9d41d969cd486c1bd7e6c7c01
05050784 <37134163+05050784@users.noreply.github.com>
2018-06-11 19:28:13 +0800

    Add files via upload

---
+++ motor.ino
@@ -0,0 +1,19 @@
+#define PinMotor1 6
+#define PinMotor2 5
+#define PinMotor3 10
+#define PinMotor4 11
+void setup() 
+{
+    pinMode(PinMotor1, OUTPUT);
+    pinMode(PinMotor2, OUTPUT);
+    pinMode(PinMotor3, OUTPUT);
+    pinMode(PinMotor4, OUTPUT);
+}
+void loop()
+{
+    analogWrite(PinMotor1,120);
+    analogWrite(PinMotor2,0);
+    analogWrite(PinMotor3,120);
+    analogWrite(PinMotor4,0);
+    delay(2000);
+}
